d42961f3b5c514baa7952b27b2245a120faa15cb
luanfmendes <lna_mendes@hotmail.com>
2025-01-02 10:08:08 -0300

    add test on main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.documents.Document;
+import org.example.factories.DocumentFactory;
+import org.example.factories.HTMLDocumentFactory;
+import org.example.factories.PDFDocumentFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DocumentFactory pdfFactory = new PDFDocumentFactory();
+        Document pdfInvoice = pdfFactory.createInvoice();
+        pdfInvoice.generate();
+
+        DocumentFactory htmlFactory = new HTMLDocumentFactory();
+        Document htmlReport = htmlFactory.createReport();
+        htmlReport.generate();
     }
 }
